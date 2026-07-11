7115298b4cecd67acd6d4af711423abb1b688cf4
sriiatharva-lang <2405188@kiit.ac.in>
2026-07-11 14:22:18 +0530

    Implement a REPL [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,11 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
-        // Stage 1: Print a prompt
-        System.out.print("$ ");
-
-        // Stage 2: Handle invalid commands
         Scanner scanner = new Scanner(System.in);
-        String input = scanner.nextLine();
-
-        System.out.println(input + ": command not found");
+        while(true){
+            System.out.print("$ ");
+            String input = scanner.nextLine();
+            System.out.println(input + " is not a valid command");
+        }
     }
 }
