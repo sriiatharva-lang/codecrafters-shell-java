2a8b06f9dfe8b037c68a55d9868a5678db80f7c4
sriiatharva-lang <2405188@kiit.ac.in>
2026-07-11 14:01:15 +0530

    Handle invalid commands [skip ci]

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,13 @@
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) throws Exception {
+        // Stage 1: Print a prompt
         System.out.print("$ ");
+
+        // Stage 2: Handle invalid commands
+        Scanner scanner = new Scanner(System.in);
+        String input = scanner.nextLine();
+
+        System.out.println(input + ": command not found");
     }
 }
